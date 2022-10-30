19d549117a973866d16b5d88ba33abf91ad22915
Alyona <alyona.kirushcheva@gmail.com>
2022-10-31 00:32:02 +0300

    task5_3

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+function outputNum(int $A, int $B)
+{
+    if ($A == $B) {
+        echo $B . "</br>";
+    } else if ($A < $B) {
+        echo $A++ . ", ";
+        outputNum($A++, $B);
+    } else {
+        echo $A-- . ", ";
+        outputNum($A--, $B);
+    }
+}
+
+?>
+
+
+
