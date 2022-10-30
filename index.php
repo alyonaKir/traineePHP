664343240b40d4944f29731937c9f20aee8bcb9c
Alyona <alyona.kirushcheva@gmail.com>
2022-10-31 00:09:18 +0300

    task3_2

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-    function singleDigit($inputNum){
+function singleDigit($inputNum)
+{
+    if ($inputNum < 0) {
+        throw new Exception("Negative value");
+    } else {
         $sum = 0;
-        while(strlen((string)$inputNum)!=1){
+        while (strlen((string)$inputNum) != 1) {
             $sum = 0;
-            while($inputNum!=0) {
+            while ($inputNum != 0) {
                 $sum += $inputNum % 10;
                 $inputNum = intdiv($inputNum, 10);
             }
@@ -11,4 +15,8 @@
         }
         return $sum;
     }
+}
+
 ?>
+
+
