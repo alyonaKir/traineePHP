50b83df9346256aac20cd0d752d024a8ccb321a3
Alyona <alyona.kirushcheva@gmail.com>
2022-10-31 13:10:29 +0300

    task4_3

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
-    function deleteNth(array $a, int $position): array{
-        $buff = $a;
-        for($i=$position-1; $i<count($buff)-1; $i++){
-            $buff[$i] = $buff[$i+1];
-        }
-        unset($buff[count($buff)-1]);
-        return $buff;
+function deleteNth(array $a, int $position): array
+{
+    $buff = $a;
+    for ($i = $position; $i < count($buff) - 1; $i++) {
+        $buff[$i] = $buff[$i + 1];
     }
+    unset($buff[count($buff) - 1]);
+    return $buff;
+}
 
 ?>
+
 
